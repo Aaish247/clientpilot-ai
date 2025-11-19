98667d15f7a9a812276061670cf3566b903984aa
Aaish247 <muhammadaaish0007@gmail.com>
2025-11-19 13:50:42 +0500

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
-      <h1>ClientPilot AI</h1>
-      <p>Welcome to your new AI tool.</p>
-    </div>
-  );
-}
